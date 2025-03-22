a59ee594757a29741e3bbbd87f2e25b36d2487d9
mleinad <101401882+mleinad@users.noreply.github.com>
2025-03-22 17:32:05 +0000

    refactor

--- Assets/Scenes/Scripts/Enemy States/PatrolState.cs
+++ Assets/Scenes/Scripts/Enemy States/PatrolState.cs
@@ -4,17 +4,17 @@
     {
         public override void EnterState(EnemyBehaviour context)
         {
-            throw new System.NotImplementedException();
+            
         }
 
         public override void UpdateState(EnemyBehaviour context)
         {
-            throw new System.NotImplementedException();
+            
         }
 
         public override void ExitState(EnemyBehaviour context)
         {
-            throw new System.NotImplementedException();
+            
         }
     }
 }
--- Assets/Scenes/Scripts/EnemyBehaviour.cs
+++ Assets/Scenes/Scripts/EnemyBehaviour.cs
@@ -1,7 +1,7 @@
-using System.Collections;
 using System.Collections.Generic;
 using Scenes.Scripts.Enemy_States;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyBehaviour : MonoBehaviour
 { 
@@ -12,17 +12,66 @@ public class EnemyBehaviour : MonoBehaviour
     public IddleState iddleState = new IddleState();
     public TeleportState teleportState = new TeleportState();
     public PatrolState patrolState = new PatrolState();
+    
+    private NavMeshAgent _agent;
+    private Transform _target;
 
-
+    [SerializeField]
+    private float speed = 10;
+    
+    [SerializeField]
+    List<Transform> waypoints = new List<Transform>();
+        
     void Start()
     {
+        _target = PlayerController.Instance.transform;
         
+        _agent = GetComponent<NavMeshAgent>();    
+        
+        currentState = patrolState;
         currentState.EnterState(this);
     }
 
 
     void Update()
     {
+        currentState.UpdateState(this);
         
     }
+
+   public Transform GetPlayerTransform() => PlayerController.Instance.transform;
+
+
+   #region Navigation
+   public void ChasePlayer()
+   {
+       speed = 20;
+       _agent.destination = _target.position;
+   }
+
+
+   public void StopMoving()
+   {
+       speed = 0;
+       _agent.speed = 0f;
+   }
+   public void SetPatrol()
+   {
+       if(waypoints.Count > 0)
+       {
+           speed = 10;
+           int point = Random.Range(0, waypoints.Count);
+           _agent.SetDestination(waypoints[point].position);
+       }
+       else
+       {
+           Debug.Log("No waypoints in list!");
+       }
+   }
+   
+   
+   
+   
+   
+   #endregion
 }
